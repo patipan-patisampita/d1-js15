f66e1ba5204cfcc32a6eb0712f95cc610ace1bf0
patipan-patisampita <patipan.patisampita@gmail.com>
2021-09-21 13:04:37 +0700

    update

---
+++ js/trycat.js
@@ -0,0 +1,12 @@
+try {
+  ("use strict");
+  var x = 5;
+  var y = 3;
+  var z = x + y;
+  document.writeln(`${x}+ ${y} = ${z}`);
+} catch (err) {
+  document.write(err);
+  throw " You can not do that";
+} finally {
+  document.writeln("document error");
+}
